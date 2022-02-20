daab28933c0ce66b0f07578de26d5063eb359c08
Harsh Mishra <harsh18mishra@gmail.com>
2022-02-20 21:08:53 +0530

    removed unnecessary comments

--- src/routers/task.js
+++ src/routers/task.js
@@ -77,7 +77,6 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-
         const task = await Task.findOneAndDelete({
             _id: req.params.id,
             owner: req.user._id,
